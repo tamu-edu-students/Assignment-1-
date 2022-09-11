708df3741a012a4dbee8f85550f2bc2eb08a03ef
EricYeh95128 <108774972+EricYeh95128@users.noreply.github.com>
2022-09-11 15:16:28 -0500

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,20 +4,51 @@
 
 def sum(arr)
   # YOUR CODE HERE
+  if arr.size == 0
+    return 0
+  else
+    return(arr.sum)
+  end
+  
 end
 
 def max_2_sum(arr)
   # YOUR CODE HERE
+  if arr.size == 0
+    return 0
+  elsif arr.size == 1
+    return arr[0]
+  else
+    arr = arr.sort
+    arr = arr.reverse
+    return(arr[0]+arr[1])
+  end
 end
 
 def sum_to_n?(arr, number)
   # YOUR CODE HERE
+  arr = arr.sort 
+  # two pointer
+  left = 0
+  right = arr.size-1
+  while left < right
+    add = arr[left] + arr[right]
+    if add < number
+      left += 1
+    elsif add > number
+      right -= 1
+    else
+      return true
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  return "Hello, " + String.new(name)
 end
 
 def starts_with_consonant?(string)
